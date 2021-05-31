483475b98c554c72b436eeec1428fead20df8dd4
weiha <weihas@icloud.com>
2021-05-31 22:25:44 +0800

    Initial Commit

---
+++ Flip/FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipApp.swift
+//  Flip
+//
+//  Created by WeIHa'S on 2021/5/30.
+//
+
+import SwiftUI
+
+@main
+struct FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
